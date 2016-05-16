d23440e77a8152e088e1f5ff178da72b87af1dbe
QiuQiu <ilsh1022@gmail.com>
2016-05-16 15:54:23 +0800

    first commit for mobile pay params

---
+++ src/client.go
@@ -0,0 +1,433 @@
+package alipay
+
+import (
+	"log"
+	"sort"
+	"strings"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"crypto/sha1"
+	"time"
+)
+
+const (
+	apigate = "https://openapi.alipay.com/gateway.do"
+)
+
+//http://stackoverflow.com/questions/20655702/signing-and-decoding-with-rsa-sha-in-go
+// loadPrivateKey loads an parses a PEM encoded private key file.
+func loadPublicKey(path string) (Unsigner, error) {
+	return parsePublicKey([]byte(`-----BEGIN PUBLIC KEY-----
+your public key here
+-----END PUBLIC KEY-----`))
+}
+
+// parsePublicKey parses a PEM encoded private key.
+func parsePublicKey(pemBytes []byte) (Unsigner, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("ssh: no key found")
+	}
+
+	var rawkey interface{}
+	switch block.Type {
+	case "PUBLIC KEY":
+		rsa, err := x509.ParsePKIXPublicKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		rawkey = rsa
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+	}
+
+	return newUnsignerFromKey(rawkey)
+}
+
+// loadPrivateKey loads an parses a PEM encoded private key file.
+func loadPrivateKey(path string) (Signer, error) {
+	pem, e := ioutil.ReadFile(path)
+	if e != nil {
+		return nil, e
+	}
+	return parsePrivateKey(pem)
+//	return parsePrivateKey([]byte(`-----BEGIN RSA PRIVATE KEY-----
+//	your private pem here
+//	bababababaab
+//-----END RSA PRIVATE KEY-----`))
+}
+
+// parsePublicKey parses a PEM encoded private key.
+func parsePrivateKey(pemBytes []byte) (Signer, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil {
+		return nil, errors.New("ssh: no key found")
+	}
+
+	var rawkey interface{}
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		rsa, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		rawkey = rsa
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %q", block.Type)
+	}
+	return newSignerFromKey(rawkey)
+}
+
+// A Signer is can create signatures that verify against a public key.
+type Signer interface {
+	// Sign returns raw signature for the given data. This method
+	// will apply the hash specified for the keytype to the data.
+	Sign(data []byte) ([]byte, error)
+}
+
+// A Signer is can create signatures that verify against a public key.
+type Unsigner interface {
+	// Sign returns raw signature for the given data. This method
+	// will apply the hash specified for the keytype to the data.
+	Unsign(data[]byte, sig []byte) error
+}
+
+func newSignerFromKey(k interface{}) (Signer, error) {
+	var sshKey Signer
+	switch t := k.(type) {
+	case *rsa.PrivateKey:
+		sshKey = &rsaPrivateKey{t}
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
+	}
+	return sshKey, nil
+}
+
+func newUnsignerFromKey(k interface{}) (Unsigner, error) {
+	var sshKey Unsigner
+	switch t := k.(type) {
+	case *rsa.PublicKey:
+		sshKey = &rsaPublicKey{t}
+	default:
+		return nil, fmt.Errorf("ssh: unsupported key type %T", k)
+	}
+	return sshKey, nil
+}
+
+type rsaPublicKey struct {
+	*rsa.PublicKey
+}
+
+type rsaPrivateKey struct {
+	*rsa.PrivateKey
+}
+
+// Sign signs data with rsa-sha256
+func (r *rsaPrivateKey) Sign(data []byte) ([]byte, error) {
+	h := sha1.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	return rsa.SignPKCS1v15(rand.Reader, r.PrivateKey, crypto.SHA1, d)
+}
+
+// Unsign verifies the message using a rsa-sha256 signature
+func (r *rsaPublicKey) Unsign(message []byte, sig []byte) error {
+	h := sha1.New()
+	h.Write(message)
+	d := h.Sum(nil)
+	return rsa.VerifyPKCS1v15(r.PublicKey, crypto.SHA1, d, sig)
+}
+
+type ZhifubaoApiClient struct {
+	Pid            string
+	PrivatePemPath string
+}
+
+var (
+	//charset = "utf-8"
+	//sightype = "RSA"
+	//version = "1.0"
+	publicParams = map[string]string{
+		//"charset": "utf-8",
+		"_input_charset": "utf-8",
+		"sign_type": "RSA",
+		"version": "1.0",
+	}
+)
+
+func CreateZhifubaoClient(pid, pemPath string) (client *ZhifubaoApiClient) {
+	client = &ZhifubaoApiClient{
+		Pid:pid,
+		PrivatePemPath:pemPath,
+		//PrivatePemPath:"/Users/james/Dropbox/HAVE/zhifubao/rsa_private_key.pem",
+	}
+	return
+}
+
+func (c *ZhifubaoApiClient) sign(params map[string]string) (secret string) {
+	params["timestamp"] = time.Now().Format("2006-01-02 15:04:05")
+	keys := []string{}
+	for k, _ := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	kv := []string{}
+	for _, k := range keys {
+		kv = append(kv, k + "=" + params[k])
+	}
+	signer, err := loadPrivateKey(c.PrivatePemPath)
+	if err != nil {
+		return
+	}
+	signed, err := signer.Sign([]byte(strings.Join(kv, "&")))
+	if err != nil {
+		fmt.Errorf("could not sign request: %v", err)
+	}
+	secret = url.QueryEscape(base64.StdEncoding.EncodeToString(signed))
+	return
+
+}
+
+func (c *ZhifubaoApiClient) CreateOrderByOrderId(ordId string) (res bool, e error) {
+	params := publicParams
+	params["app_id"] = c.Pid
+	params["method"] = "alipay.trade.create"
+	params["out_trade_no"] = ordId
+	params["total_amount"] = "0.01"
+	params["buyer_logon_id"] = ""
+	//params["method"] = "alipay.pass.instance.update"
+	//params["serial_number"] = ordId
+	//params["channel_id"] = ordId
+	params["sign"] = c.sign(params)
+	form := url.Values{}
+	for k, v := range params {
+		form.Add(k, v)
+	}
+	log.Println(form)
+	req, err := http.NewRequest("POST", apigate, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println("a")
+		log.Println(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("b")
+		log.Println(err)
+		return
+	}
+	var dat map[string]interface{}
+	json.Unmarshal(body, &dat)
+	log.Println("c")
+	log.Println(dat)
+	return
+}
+
+func (c *ZhifubaoApiClient) QueryPayResByOrderId(ordId string) (res bool, e error) {
+	params := map[string]string{
+		"_input_charset": "utf-8",
+		"service" : "single_trade_query",
+		"partner" : c.Pid,
+		"out_trade_no": ordId,
+	}
+
+	params["sign"] = c.sign(params)
+	form := url.Values{}
+	for k, v := range params {
+		form.Add(k, v)
+	}
+	req, err := http.NewRequest("POST", apigate, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println("a")
+		log.Println(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("b")
+		log.Println(err)
+		return
+	}
+	var dat map[string]interface{}
+	json.Unmarshal(body, &dat)
+	log.Println("c")
+	log.Println(dat)
+	//log.Println(dat["alipay_trade_query_response"].(map[string]interface{})["sub_msg"])
+	//log.Println(dat["alipay_pass_instance_update_response"].(map[string]interface{})["sub_msg"])
+	return
+	//array := []string{}
+	//for k, v := range params {
+	//	array = append(array, k + "=" + ("\"" + v + "\""))
+	//}
+	//signer, err := loadPrivateKey(c.PrivatePemPath)
+	//if err != nil {
+	//	return ""
+	//}
+	//signed, err := signer.Sign([]byte(strings.Join(array, "&")))
+	//if err != nil {
+	//	fmt.Errorf("could not sign request: %v", err)
+	//}
+	//secret := url.QueryEscape(base64.StdEncoding.EncodeToString(signed))
+	//log.Println("afefef")
+	//log.Println(strings.Join(array, "&") + "&sign=\"" + secret + "\"&sign_type=\"RSA\"")
+	//return strings.Join(array, "&") + "&sign=" + secret + "&sign_type=\"RSA\""
+	//return
+}
+
+//https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.aQc9uV&treeId=59&articleId=103663&docType=1
+func (c *ZhifubaoApiClient) GetMobilePayOrderString(ordId, price, notifyUrl, subject, detail string) (string) {
+	params := map[string]string{
+		"_input_charset": "utf-8",
+		"service" : "mobile.securitypay.pay",
+		"partner" : c.Pid,
+		"notify_url": notifyUrl,
+		"out_trade_no": ordId,
+		"subject": subject,
+		"payment_type": "1",
+		"seller_id": c.Pid,
+		"total_fee": price,
+		"body": detail,
+	}
+	array := []string{}
+	for k, v := range params {
+		array = append(array, k + "=" + ("\"" + v + "\""))
+	}
+	signer, err := loadPrivateKey(c.PrivatePemPath)
+	if err != nil {
+		return ""
+	}
+	signed, err := signer.Sign([]byte(strings.Join(array, "&")))
+	if err != nil {
+		fmt.Errorf("could not sign request: %v", err)
+	}
+	secret := url.QueryEscape(base64.StdEncoding.EncodeToString(signed))
+	log.Println("afefef")
+	log.Println(strings.Join(array, "&") + "&sign=\"" + secret + "\"&sign_type=\"RSA\"")
+	return strings.Join(array, "&") + "&sign=" + secret + "&sign_type=\"RSA\""
+}
+
+func (c *ZhifubaoApiClient) PayByOrderId(ordId string) (res bool, e error) {
+	params := publicParams
+	params["app_id"] = c.Pid
+	params["service"] = "mobile.securitypay.pay"
+	params["partner"] = c.Pid
+	params["notify_url"] = "url"
+	params["out_trade_no"] = ordId
+	params["subject"] = "this is a subject"
+	params["payment_type"] = "1"
+	params["seller_id"] = c.Pid
+	params["total_fee"] = "0.01"
+	params["body"] = "detail"
+	//params["method"] = "alipay.pass.instance.update"
+	//params["serial_number"] = ordId
+	//params["channel_id"] = ordId
+
+	//params["method"] = "alipay.trade.create"
+	//params["out_trade_no"] = ordId
+	//params["total_amount"] = "0.01"
+	//params["buyer_logon_id"] = "15921622336"
+	params["sign"] = c.sign(params)
+	form := url.Values{}
+	for k, v := range params {
+		form.Add(k, v)
+	}
+	req, err := http.NewRequest("POST", apigate, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println("a")
+		log.Println(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("b")
+		log.Println(err)
+		return
+	}
+	var dat map[string]interface{}
+	json.Unmarshal(body, &dat)
+	log.Println("c")
+	log.Println(dat)
+	//log.Println(dat["alipay_trade_query_response"].(map[string]interface{})["sub_msg"])
+	//log.Println(dat["alipay_pass_instance_update_response"].(map[string]interface{})["sub_msg"])
+	return
+}
+
+func (c *ZhifubaoApiClient) QueryOrderByOrderId(ordId string) (res bool, e error) {
+	params := publicParams
+	params["app_id"] = c.Pid
+	params["method"] = "alipay.trade.query"
+	params["out_trade_no"] = ordId
+
+	//params["method"] = "alipay.pass.instance.update"
+	//params["serial_number"] = ordId
+	//params["channel_id"] = ordId
+
+	//params["method"] = "alipay.trade.create"
+	//params["out_trade_no"] = ordId
+	//params["total_amount"] = "0.01"
+	//params["buyer_logon_id"] = "15921622336"
+	params["sign"] = c.sign(params)
+	form := url.Values{}
+	for k, v := range params {
+		form.Add(k, v)
+	}
+	log.Println(form)
+	req, err := http.NewRequest("POST", apigate, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Println("a")
+		log.Println(err)
+		return
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("b")
+		log.Println(err)
+		return
+	}
+	var dat map[string]interface{}
+	json.Unmarshal(body, &dat)
+	log.Println("c")
+	log.Println(dat)
+	log.Println(dat["alipay_trade_query_response"].(map[string]interface{})["sub_msg"])
+	//log.Println(dat["alipay_pass_instance_update_response"].(map[string]interface{})["sub_msg"])
+	return
+}
+
+func (c *ZhifubaoApiClient) QueryOrderByTradeId(tradeId string) (res bool, e error) {
+	return
+}
+
+//func main() {
+//
+//	client := CreateZhifubaoClient("your pid", "your path to private pem")
+//	//back := client.GetMobilePayOrderString("id", "0.01", "url", "subject", "detail")
+
+//}
+
+
